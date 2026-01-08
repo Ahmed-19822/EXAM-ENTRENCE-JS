6a7a02da040e2eef37ed3769acef319c931dc9bc
Hasanhayat <aylskhsmvz.v@gmail.com>
2026-01-08 20:00:16 +0500

    Updated files

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
 let age = (prompt("ENTER YOUR AGE"))
-let admit = prompt("Do You Have Admit Card?  (YES/NO)")
-let fee = prompt("Have You Paid Fees? (YES/NO)")
-let late = prompt("If You Are Late Tell Us Late Minutes")
-let war = prompt("Tell Us How Many Warning Have You Recived")
-
  if ( age>18 && age<99 ){
     // allow
 }
 else if (age<18){
     alert(" Warning! You Are Under Age")
 }
-
+let admit = prompt("Do You Have Admit Card?  (YES/NO)")
 if (admit === "yes"){
     //allow
 }
+
 else if (admit === "no"){
     alert(" Warning! dont have admit card ")
 }
-
+let fee = prompt("Have You Paid Fees? (YES/NO)")
 if (fee === "yes"){
     //allow
 }
 else if (fee === "no"){
     alert(" Warning! You have not paid fees ")
 }
+let late = prompt("If You Are Late Tell Us Late Minutes")
 
 if (late<2){
     //allow
@@ -31,8 +28,10 @@ if (late<2){
 else if (late>2){
     alert(" Warning! you are late")
 }
+
+let war = prompt("Tell Us How Many Warning Have You Recived")
 if (war<1){
-    //allow
+    alert("Entry allowed with Good Status")
 }
 else if (war<2){
     alert("Entry Allowed with Warning")
@@ -41,4 +40,4 @@ else if (war<2){
 else if (war>2){
     alert("Entry denied due to missing requirement")
 }
-  
+
